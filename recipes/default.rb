16db0e08814479f13c348092345e0a0efaa1d4e6
Lacina ZINA <zinalacina@gmail.com>
2019-04-18 21:22:39 +0900

    First Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+execute 'apt-get update' do
+    command 'apt-get update'
+end
+
+package 'apache2' do
+    action :install
+end
+
+service 'apache2' do
+    action [:start, :enable]
+end
+
+cookbook_file '/var/www/html' do
+    source 'index.html'
+    mode '0644'
+    action :create
+end
